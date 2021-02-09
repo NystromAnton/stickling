0a1dc6543ee7eb699f5cea85c600d8215dbd0991
Oscar Englöf <oscar.englof@hotmail.com>
2021-02-09 11:34:33 +0100

    -

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get("/", function (req, res, next) {
-  res.send("Hej Galadrius");
+  res.send("respond with a resource");
 });
 router.post("/", function (req, res, next) {
   res.send("respond with a resource");
