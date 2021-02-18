90d70da62e7d7154ec8d5d12557e6fd4e7ea49d0
jdaawg <35804066+jdaawg@users.noreply.github.com>
2021-02-18 14:21:15 +0100

    Add plant to database
    Now we can add plants to the database, but needs to be connected to frontend
    
    Co-Authored-By: Oscar Englöf <36447217+ralfkatt@users.noreply.github.com>
    Co-Authored-By: Anton Nyström <36476115+NystromAnton@users.noreply.github.com>

--- backend/app.js
+++ backend/app.js
@@ -7,6 +7,8 @@ require("dotenv").config();
 
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
+var plantRouter = require("./routes/plants");
+
 
 var app = express();
 
@@ -24,6 +26,7 @@ app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
+app.use("/plants", plantRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
---
+++ backend/models/plantModel.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+
+const PlantSchema = new mongoose.Schema({
+  user: {
+    type: String,
+    required: true,
+  },
+  title: {
+    type: String,
+    required: true,
+  },
+  desc: {
+    type: String,
+  },
+  pic: {
+    type: String,
+    required: true,
+  },
+  type: {
+    type: String,
+  },
+});
+module.exports = Plant = mongoose.model("plants", PlantSchema);
