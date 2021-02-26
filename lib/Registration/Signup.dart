f2574cfd8d8f4dac2ee9544b32fd05cf744bc79b
Umer Farooq <umerfarooqsaeed7@gmail.com>
2021-02-26 08:23:36 +0100

    Adding apicallMethods

--- lib/Registration/Signup.dart
+++ lib/Registration/Signup.dart
@@ -29,8 +29,30 @@ class _SignupState extends State<Signup> {
     passwordController.dispose();
     super.dispose();
   }
-
+  Future<String> apiRequest(String url, Map jsonMap) async {
+    HttpClient httpClient = new HttpClient();
+    HttpClientRequest request = await httpClient.postUrl(Uri.parse(url));
+    request.headers.set('content-type', 'application/json');
+    request.add(utf8.encode(json.encode(jsonMap)));
+    HttpClientResponse response = await request.close();
+    // todo - you should check the response.statusCode
+    String reply = await response.transform(utf8.decoder).join();
+    httpClient.close();
+    return reply;
+  }
   var isEnabled = false;
+  String url =
+      'http://localhost:3000/users/signup';
+  Map map = {
+    "name": "",
+    "email":"abc@gmail.com",
+    "Password":"12345",
+    "location":{
+      "cordinates":(12345,12345)
+    }
+  };
+
+  // print(await apiRequest(url, map));
   @override
   Widget build(BuildContext context) {
     return Scaffold(
