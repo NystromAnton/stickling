70e376e49f298d2f836e5f9709b9f43e54a50134
Oscar Englöf <oscar.englof@hotmail.com>
2021-03-10 10:12:58 +0100

    preferences

--- backend/routes/preferences.js
+++ backend/routes/preferences.js
@@ -5,33 +5,44 @@ const Plant = require("../models/plantModel");
 const Preference = require("../models/preferenceModel");
 
 router.get("/:id", async function (req, res, next) {
-    var id = req.params.id;
-    var preference = await Preference.find({user: id});
-    res.json(preference);
+  var id = req.params.id;
+  var preference = await Preference.find({ user: id });
+  res.json(preference);
 });
 
 router.post("/add-preference", async function (req, res, next) {
-    var form = req.body;
-    var userId = form.id;
-    try {
-      let user = await User.findById(userId);
-      if (user) {
-        
-      const newPreference = new Preference({
-        user: userId,
-        types: form.types,
-        distance: form.distance,
-      });
-  
-      await newPreference.save();
-      res.send(newPreference);
+  var form = req.body;
+  var userId = form.id;
+  try {
+    let user = await User.findById(userId);
+    if (user) {
+      var preference = await Preference.findOne({ user: userId });
+      console.log(preference);
+      if (preference) {
+        var p = await Preference.findOneAndUpdate(
+          { _id: preference._id },
+          {
+            $set: { types: form.types, distance: form.distance },
+          }
+        );
+        console.log(p);
+        res.send("Preference updated");
+      } else {
+        const newPreference = new Preference({
+          user: userId,
+          types: form.types,
+          distance: form.distance,
+        });
+
+        await newPreference.save();
+        res.send(newPreference);
       }
-      else {
-      res.send("User doesn't exist")
-      }
-    } catch (err) {
-      console.error(err.message);
-      res.status(500).json({ message: "Server error" });
+    } else {
+      res.send("User doesn't exist");
     }
-  });
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).json({ message: "Server error" });
+  }
+});
 module.exports = router;
