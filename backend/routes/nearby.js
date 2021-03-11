722fc4315453bd390eae4ddc0ea78dc16e7e4be0
Oscar Englöf <oscar.englof@hotmail.com>
2021-03-11 17:31:58 +0100

    -

--- backend/routes/nearby.js
+++ backend/routes/nearby.js
@@ -6,70 +6,67 @@ const Match = require("../models/matchModel");
 const Preference = require("../models/preferenceModel");
 const mongoose = require("mongoose");
 
+router.get("/:id", async function (req, res, next) {
+  var coord = req.query.q;
+  var splitCoord = coord.split(",");
+  var long = parseFloat(splitCoord[0]);
+  var lat = parseFloat(splitCoord[1]);
+  console.log(lat);
+  console.log(long);
 
-router.get("/:id", async function(req, res, next){
-    var coord = req.query.q;
-    var splitCoord = coord.split(",");
-    var long = parseFloat(splitCoord[0]);
-    var lat = parseFloat(splitCoord[1]);
-    console.log(lat);
-    console.log(long);
+  var id = req.params.id;
+  console.log(id);
+  var preference = await Preference.find({ user: id });
+  console.log(preference);
+  var types = preference[0].types;
+  var distance = preference[0].distance;
+  User.aggregate(
+    [
+      {
+        $geoNear: {
+          near: {
+            type: "Point",
+            coordinates: [long, lat],
+          },
+          distanceField: "dist.calculated",
+          maxDistance: Number(distance),
+          spherical: true,
+        },
+      },
 
-    var id = req.params.id;
-    console.log(id)
-    var preference = await Preference.find({ user: id });
-    console.log(preference)
-    var types = preference[0].types;
-    var distance = preference[0].distance;
-    User.aggregate([
-        {  
-            $geoNear: {
-                near: {
-                    type: "Point",
-                    coordinates: [long, lat],
-                },
-                distanceField: "dist.calculated",
-                maxDistance: Number(distance),
-                spherical: true,
-                },
-            },
-            
-            {
+      {
+        $lookup: {
+          from: Plant.collection.name,
+          localField: "_id",
+          foreignField: "user",
+          as: "plants",
+        },
+      },
 
-                $lookup: {
-                    from: Plant.collection.name,
-                    localField: "_id",
-                    foreignField: "user",
-                    as: "plants",
-                    
-                },
-            },
-            
-            { $unwind: "$plants" } ,
-            {
-                $match: { "plants.type": { $in: types } }
-             },
-    
-            {
-                $project: {
-                    _id: 0,
-                    plants: 1,
-                },
-            },
+      { $unwind: "$plants" },
+      {
+        $match: { "plants.type": { $in: types }, "plants.user": { $ne: id } },
+      },
+      {
+        $project: {
+          _id: 0,
+          plants: 1,
+        },
+      },
     ],
-    (err, data)=> {
-        if(err) {
-            next(err);
-            return;
-        }
-        var arr = [];
-        for(i = 0; i < data.length; i++) {
-            arr.push(data[i].plants);
-            
-        }
-        console.log(arr);
-        res.json(arr);
-    })
+    (err, data) => {
+      if (err) {
+        next(err);
+        return;
+      }
+      var arr = [];
+      for (i = 0; i < data.length; i++) {
+        arr.push(data[i].plants);
+      }
+      console.log(arr);
+      res.json(arr);
+    }
+  );
 });
 
 module.exports = router;
