1fc339139e5c33cecabfa470ee5e4a4db8b93259
jdaawg <35804066+jdaawg@users.noreply.github.com>
2021-09-29 15:33:17 +0200

    Chat view and messages

--- lib/Chat/AllChatsPage.dart
+++ lib/Chat/AllChatsPage.dart
@@ -5,6 +5,7 @@ import './User.dart';
 import 'package:http/http.dart' as http;
 import 'package:flutter/cupertino.dart';
 import 'dart:convert';
+import 'ChatPage.dart';
 
 class AllChatsPage extends StatefulWidget {
   String currentUserID;
@@ -45,82 +46,104 @@ class _AllChatsPageState extends State<AllChatsPage> {
                       ));
                     } else {
                       List chatRooms = snapshot.data;
-
                       return Container(
                         height: 500,
                         child: ListView.builder(
-                          itemCount: chatRooms.length,
-                          itemBuilder: (context, i) {
-                            String whichUser;
-                            if (chatRooms[i]["user1"]["_id"] ==
-                                widget.currentUserID) {
-                              whichUser = "user1";
-                            } else {
-                              whichUser = "user2";
-                            }
-
-                            return Padding(
-                              padding: const EdgeInsets.all(10.0),
-                              child: Container(
-                                height: 100,
-                                width: 150,
-                                color: Colors.white,
-                                child: Row(
-                                  children: [
-                                    Expanded(
-                                      child: Column(
-                                        mainAxisAlignment:
-                                            MainAxisAlignment.start,
-                                        crossAxisAlignment:
-                                            CrossAxisAlignment.start,
-                                        children: <Widget>[
-                                          Padding(
-                                            padding: const EdgeInsets.only(
-                                              bottom: 8.0,
-                                              right: 10,
-                                              left: 20,
-                                            ),
-                                            child: Row(
-                                              mainAxisAlignment:
-                                                  MainAxisAlignment
-                                                      .spaceBetween,
-                                              crossAxisAlignment:
-                                                  CrossAxisAlignment.start,
-                                              children: [
-                                                Text(
-                                                  chatRooms[i][whichUser]
-                                                      ["name"],
+                            itemCount: chatRooms.length,
+                            itemBuilder: (context, i) {
+                              String otherUser;
+                              String meUser;
+                              String message = "";
+                              if (chatRooms[i]["user1"]["_id"] ==
+                                  widget.currentUserID) {
+                                otherUser = "user1";
+                                meUser = "user2";
+                              } else {
+                                otherUser = "user2";
+                                meUser = "user1";
+                              }
+                              if (chatRooms[i]["recent_chat"] != null) {
+                                message =
+                                    chatRooms[i]["recent_chat"]["message"];
+                              }
+                              return GestureDetector(
+                                onTap: () {
+                                  Navigator.push(
+                                    context,
+                                    MaterialPageRoute(
+                                      builder: (context) => ChatPage(
+                                        chatRooms[i]["_id"],
+                                        widget.currentUserID,
+                                        chatRooms[i][otherUser]["name"],
+                                        chatRooms[i][meUser]["name"],
+                                      ),
+                                    ),
+                                  );
+                                },
+                                child: new Padding(
+                                  padding: const EdgeInsets.all(10.0),
+                                  child: Container(
+                                    height: 100,
+                                    width: 150,
+                                    color: Colors.white,
+                                    child: Row(
+                                      children: [
+                                        Expanded(
+                                          child: Column(
+                                            mainAxisAlignment:
+                                                MainAxisAlignment.start,
+                                            crossAxisAlignment:
+                                                CrossAxisAlignment.start,
+                                            children: <Widget>[
+                                              Padding(
+                                                padding: const EdgeInsets.only(
+                                                  bottom: 8.0,
+                                                  right: 10,
+                                                  left: 20,
+                                                ),
+                                                child: Row(
+                                                  mainAxisAlignment:
+                                                      MainAxisAlignment
+                                                          .spaceBetween,
+                                                  crossAxisAlignment:
+                                                      CrossAxisAlignment.start,
+                                                  children: [
+                                                    Text(
+                                                      chatRooms[i][otherUser]
+                                                          ["name"],
+                                                      style: TextStyle(
+                                                          fontFamily: 'Lato',
+                                                          fontWeight:
+                                                              FontWeight.bold,
+                                                          color: Colors.black,
+                                                          fontSize: 20),
+                                                    ),
+                                                  ],
+                                                ),
+                                              ),
+                                              Padding(
+                                                padding:
+                                                    EdgeInsets.only(left: 20),
+                                                child: Text(
+                                                  message,
+                                                  maxLines: 2,
                                                   style: TextStyle(
                                                       fontFamily: 'Lato',
                                                       fontWeight:
-                                                          FontWeight.bold,
+                                                          FontWeight.normal,
                                                       color: Colors.black,
                                                       fontSize: 20),
                                                 ),
-                                              ],
-                                            ),
+                                              ),
+                                            ],
                                           ),
-                                          Padding(
-                                            padding: EdgeInsets.only(left: 20),
-                                            child: Text(
-                                              chatRooms[i]["recent_chat"],
-                                              maxLines: 2,
-                                              style: TextStyle(
-                                                  fontFamily: 'Lato',
-                                                  fontWeight: FontWeight.normal,
-                                                  color: Colors.black,
-                                                  fontSize: 20),
-                                            ),
-                                          ),
-                                        ],
-                                      ),
-                                    )
-                                  ],
+                                        )
+                                      ],
+                                    ),
+                                  ),
                                 ),
-                              ),
-                            );
-                          },
-                        ),
+                              );
+                            }),
                       );
                     }
                   },
@@ -133,95 +156,3 @@ class _AllChatsPageState extends State<AllChatsPage> {
     );
   }
 }
-
-/*
-import 'dart:async';
-import 'dart:convert';
-
-import 'package:flutter/material.dart';
-import 'package:http/http.dart' as http;
-import 'package:stycling/Chat/ChatPage.dart';
-
-Future<List<Chat>> fetchAllChats() async {
-  final response =
-      await http.get(Uri.parse('https://sticklingar.herokuapp.com/chat/'));
-
-  if (response.statusCode == 200) {
-    // If the server did return a 200 OK response,
-    // then parse the JSON.
-    return Album.fromJson(jsonDecode(response.body));
-  } else {
-    // If the server did not return a 200 OK response,
-    // then throw an exception.
-    throw Exception('Failed to load album');
-  }
-}
-
-class Album {
-  final int userId;
-  final int id;
-  final String title;
-
-  Album({
-    required this.userId,
-    required this.id,
-    required this.title,
-  });
-
-  factory Album.fromJson(Map<String, dynamic> json) {
-    return Album(
-      userId: json['userId'],
-      id: json['id'],
-      title: json['title'],
-    );
-  }
-}
-
-void main() => runApp(const MyApp());
-
-class MyApp extends StatefulWidget {
-  const MyApp({Key? key}) : super(key: key);
-
-  @override
-  _MyAppState createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  late Future<Album> futureAlbum;
-
-  @override
-  void initState() {
-    super.initState();
-    futureAlbum = fetchAlbum();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Fetch Data Example',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Fetch Data Example'),
-        ),
-        body: Center(
-          child: FutureBuilder<ChatPage>(
-            future: futureAlbum,
-            builder: (context, snapshot) {
-              if (snapshot.hasData) {
-                return Text(snapshot.data!.title);
-              } else if (snapshot.hasError) {
-                return Text('${snapshot.error}');
-              }
-
-              // By default, show a loading spinner.
-              return const CircularProgressIndicator();
-            },
-          ),
-        ),
-      ),
-    );
-  }
-}*/
