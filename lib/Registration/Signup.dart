c2f4992841be6e1f75d2e663ca341901a6691423
Mette Nordqvist <mette.nordqvist.5557@student.uu.se>
2021-10-19 10:03:10 +0200

    Matchalgoritmen fixad
    Co-Authored-By: Helena Frestadius <35804061+pinglepingle@users.noreply.github.com>

--- lib/Registration/Signup.dart
+++ lib/Registration/Signup.dart
@@ -26,6 +26,8 @@ class _SignupState extends State<Signup> {
   final lastnameController = TextEditingController();
   final confirmPasswordController = TextEditingController();
   final passwordController = TextEditingController();
+  double lat;
+  double long;
 
   @override
   void dispose() {
@@ -37,12 +39,56 @@ class _SignupState extends State<Signup> {
     super.dispose();
   }
 
+  Future<Position> determinePosition() async {
+    bool serviceEnabled;
+    LocationPermission permission;
+
+    // Test if location services are enabled.
+    serviceEnabled = await Geolocator.isLocationServiceEnabled();
+    if (!serviceEnabled) {
+      // Location services are not enabled don't continue
+      // accessing the position and request users of the
+      // App to enable the location services.
+      return Future.error('Location services are disabled.');
+    }
+
+    permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+      if (permission == LocationPermission.deniedForever) {
+        // Permissions are denied forever, handle appropriately.
+        return Future.error(
+            'Location permissions are permanently denied, we cannot request permissions.');
+      }
+
+      if (permission == LocationPermission.denied) {
+        // Permissions are denied, next time you could try
+        // requesting permissions again (this is also where
+        // Android's shouldShowRequestPermissionRationale
+        // returned true. According to Android guidelines
+        // your App should show an explanatory UI now.
+        return Future.error('Location permissions are denied');
+      }
+    }
+
+    // When we reach here, permissions are granted and we can
+    // continue accessing the position of the device.
+    return await Geolocator.getCurrentPosition();
+  }
+
+
   Future<String> requestMethod(String url) async {
+
+    await determinePosition().then((value) {
+        this.long = value.longitude;
+        this.lat = value.latitude;
+      });
 
     var body = json.encode({
       "name": firstnameController.text,
       "email": emailController.text,
       "password": passwordController.text,
+      "location": {"type": "Point", "coordinates": [long, lat]}
     });
 
     Map<String, String> headers = {
